154be5a7354ab0ef7d7de9dc784f0fbac4ca950b
Sergio Mendes <sergiomendes@MacBook-Air-de-Sergio.local>
2024-07-15 08:33:17 -0300

    backend ok

--- tests/getCep.test.js
+++ tests/getCep.test.js
@@ -13,14 +13,12 @@ describe("GET /:CEP", () => {
     it('Should return status 200 when the zip code is valid', async () => {
         const cep = '25070-100'
         const {body} = await agent.get(`/${cep}`)
-        console.log("body 1", body)
         expect(body.status).toBe(200)
     });
 
     it('Should return status 422 when the zip code is written incorrectly', async () => {
         const cep = '2.50100'
         const {status} = await agent.get(`/${cep}`)
-        console.log("body 3", status)
         expect(status).toBe(422)
     });
 
